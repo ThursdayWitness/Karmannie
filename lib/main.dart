68682f42ff88f1d33652f087e44677060292bb46
ThursdayWitness <lirkir1256@gmail.com>
2023-03-02 12:24:01 +0500

    Небольшой рефакторинг общей темы приложения

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,78 @@
 import 'package:flutter/material.dart';
 import 'package:karmannie/pages/childLogin.dart';
-import 'package:flutter/rendering.dart';
 import 'package:karmannie/pages/parentLogin.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Karmaнные",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.amber)
+            .copyWith(background: Colors.white),
+        // buttonTheme: ,
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+              color: Colors.black,
+              fontSize: 44,
+              fontWeight: FontWeight.bold,
+              decoration: TextDecoration.none),
+        ),
+        appBarTheme: const AppBarTheme(
+          // shape: ShapeBorder(),
+          centerTitle: true,
+        ),
+      ),
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   static const String _title = 'Start page';
 
   @override
   Widget build(BuildContext context) {
-        debugPaintSizeEnabled = false;
-    return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primarySwatch: Colors.amber,
-          scaffoldBackgroundColor: const Color(0xFFCDD5FF),
+    return Builder(builder: (context) {
+      return Center(
+          child: FractionallySizedBox(
+        heightFactor: 0.3,
+        widthFactor: 0.5,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text("Karmaнные", style: Theme.of(context).textTheme.displayLarge),
+            ElevatedButton(
+              //TODO: выделить в отдельный класс
+                style: TextButton.styleFrom(
+                    fixedSize: const Size.fromWidth(200),
+                    foregroundColor: Colors.black,
+                    backgroundColor: Colors.amber),
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const ParentLogin()));
+                },
+                child: const Text("Я родитель")),
+            ElevatedButton(
+                style: TextButton.styleFrom(
+                    fixedSize: const Size.fromWidth(200),
+                    foregroundColor: Colors.black,
+                    backgroundColor: Colors.amber),
+                onPressed: () {
+                  Navigator.of(context).push(MaterialPageRoute(
+                      builder: (context) => const ChildLogin()));
+                },
+                child: const Text("Я ребёнок"))
+          ],
         ),
-        home: Builder(builder: (context) {
-          return Center(
-              child: FractionallySizedBox(
-            heightFactor: 0.3,
-            widthFactor: 0.5,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                const Text("Karmaнные",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 44,
-                        fontWeight: FontWeight.bold,
-                        decoration: TextDecoration.none)),
-                ElevatedButton(
-                    style: TextButton.styleFrom(
-                        fixedSize: const Size.fromWidth(200),
-                        foregroundColor: Colors.black,
-                        backgroundColor: Colors.amber),
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => const ParentLogin()));
-                    },
-                    child: const Text("Я родитель")),
-                ElevatedButton(
-                    style: TextButton.styleFrom(
-                        fixedSize: const Size.fromWidth(200),
-                        foregroundColor: Colors.black,
-                        backgroundColor: Colors.amber),
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(
-                          builder: (context) => const ChildLogin()));
-                    },
-                    child: const Text("Я ребёнок"))
-              ],
-            ),
-          ));
-        }));
+      ));
+    });
   }
 }
