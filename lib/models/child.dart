fcc0f2e2624ce8d76ec10b4f339a56d229799bcb
ThursdayWitness <lirkir1256@gmail.com>
2023-03-15 13:52:10 +0500

    Дополнил класс Child, сделал поля приватными

--- lib/models/child.dart
+++ lib/models/child.dart
@@ -1,10 +1,42 @@
 import 'task.dart';
 
 class Child{
-  final String name;
-  int money;
-  List<Task> tasks = List<Task>.empty(growable: true);
+  late String _name;
+  String get getName => _name;
+  late int _password;
+  int _money = 0;
+  int get getMoney => _money;
+  final List<Task> _tasks = List<Task>.empty(growable: true);
+  List<Task> get getTasks => _tasks;
 
-  Child(this.name, this.money);
-  Child.withTasks(this.name, this.money, this.tasks);
+  Child({required String name, required int password})
+  {
+    _name = name;
+    _password = password;
+  }
+
+  Child.withTasks({required String name, required int password, required List<Task>tasks})
+  {
+    _name = name;
+    _password = password;
+    _tasks.addAll(tasks);
+  }
+
+  Child.withOneTask({required String name, required int password, required Task task})
+  {
+    _name = name;
+    _password = password;
+    _tasks.add(task);
+  }
+
+  void completeTask(Task task)
+  {
+    _money += task.reward;
+    _tasks.remove(task);
+  }
+
+  void addTask(name, reward, deadline)
+  {
+    _tasks.add(Task(name, reward, deadline));
+  }
 }
