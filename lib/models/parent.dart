2ad314d887cb33097ad9429096d51d88b634caf6
ThursdayWitness <lirkir1256@gmail.com>
2023-03-15 13:54:21 +0500

    Дополнил класс Parent, добавил методы взаимодействия

--- lib/models/parent.dart
+++ lib/models/parent.dart
@@ -1,7 +1,21 @@
+import 'task.dart';
 import 'child.dart';
 
 class Parent{
-  List<Child> children;
+  final String name;
+  final String login;
+  final String password;
+  final List<Child> children;
 
-  Parent(this.children);
+  Parent(this.name, this.login, this.password, this.children);
+
+  void createChild(String name, int password)
+  {
+    children.add(Child(name: name, password: password));
+  }
+
+  void createTask(Child child, String name, int reward, DateTime deadline)
+  {
+    child.addTask(name, reward, deadline);
+  }
 }
