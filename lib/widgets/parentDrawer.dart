96121ecc590ea792f3c206ca81d8a09cef219d96
ThursdayWitness <lirkir1256@gmail.com>
2023-03-15 13:55:00 +0500

    Теперь создаётся через общий виджет Drawer

--- lib/widgets/parentDrawer.dart
+++ lib/widgets/parentDrawer.dart
@@ -1,68 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:karmannie/main.dart';
 import 'package:karmannie/pages/parentPage.dart';
 import 'package:karmannie/pages/parentTasks.dart';
+import 'package:karmannie/widgets/custom_drawer.dart';
+import '../controllers/drawer_button.dart';
 
-class ParentDrawer extends StatelessWidget {
-  const ParentDrawer({Key? key}) : super(key: key);
+class ParentDrawer extends CustomDrawer {
+  final String name;
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        DrawerHeader(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: const [
-              Icon(Icons.account_circle, size: 66),
-              Text(
-                'Имя аккаунта',
-                style: TextStyle(fontSize: 24),
-              ),
-            ],
-          ),
-        ),
-        Row(children: [
-          const Icon(Icons.fact_check),
-          TextButton(
-            onPressed: () {
-              Navigator.of(context).push(MaterialPageRoute(
-                  builder: (context) => const ParentTasks()));
-            },
-            child: const Text("Задачи", style: TextStyle(color: Colors.black)),
-          )
-        ]),
-        Row(
-          children: [
-            const Icon(Icons.groups),
-            TextButton(
-              onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(
-                    builder: (context) => const ParentPage()));
-              },
-              child: const Text("Дети", style: TextStyle(color: Colors.black)),
-            ),
-          ],
-        ),
-        Row(
-          children: [
-            const Icon(Icons.settings),
-            TextButton(
-              onPressed: () {},
-              child: const Text("Настройки",
-                  style: TextStyle(color: Colors.black)),
-            ),
-          ],
-        ),
-        Row(
-          children: [
-            const Icon(Icons.exit_to_app),
-            TextButton(
-              onPressed: () {},
-              child: const Text("Выход", style: TextStyle(color: Colors.black)),
-            ),
-          ],
-        ),
-      ],
-    );
-  }
+  const ParentDrawer(this.name, {List<DrawerButton> buttons = const [
+  DrawerButton("Задачи", Icon(Icons.fact_check), ParentTasks()),
+  DrawerButton("Дети", Icon(Icons.groups), ParentPage()),
+  DrawerButton("Выход", Icon(Icons.exit_to_app), MyApp()),
+  ], Key? key}): super(name, buttons, key: key);
 }
