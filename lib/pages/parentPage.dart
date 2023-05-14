7266f7509f9b845253adff7ba36a8df6a964d784
thursdaywitness <lirkir1256@gmail.com>
2023-05-14 14:24:36 +0500

    Добавил bottomBar родителю, в списке детей отображается их баланс

--- lib/pages/parentPage.dart
+++ lib/pages/parentPage.dart
@@ -1,5 +1,9 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:karmannie/dbUtils.dart';
 import 'package:karmannie/pages/taskPage.dart';
+import 'package:karmannie/ui/input_field.dart';
 
 import '../models/all.dart';
 import '../ui/task_box.dart';
@@ -20,7 +24,13 @@ class ParentProfile extends StatelessWidget {
           for (var child in parent.children)
             UserProfileCard(
                 userName: child.name,
-                userProfilePic: Icons.account_circle_rounded)
+                balance: child.balance.toString(),
+                userProfilePic: Icons.account_circle_rounded),
+          ElevatedButton(
+              onPressed: () {
+                addChildDialogBuilder(context);
+              },
+              child: const Text("Добавить ребёнка"))
         ]));
   }
 }
@@ -40,6 +50,32 @@ class _ParentTasksState extends State<ParentTasks> {
     return Scaffold(
       appBar: AppBar(
         title: const Text("Доска заданий"),
+      ),
+      bottomNavigationBar: Container(
+        margin: const EdgeInsets.only(left: 8.0, right: 8.0),
+        padding: const EdgeInsets.only(left: 8.0, right: 8.0),
+        decoration: BoxDecoration(
+            color: Theme.of(context).colorScheme.primary,
+            border: const Border(),
+            borderRadius: const BorderRadius.only(
+                topLeft: Radius.circular(10), topRight: Radius.circular(10))),
+        child: BottomNavigationBar(
+            elevation: 0,
+            iconSize: 30,
+            selectedItemColor: Theme.of(context).colorScheme.secondary,
+            unselectedItemColor: Theme.of(context).colorScheme.secondary,
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            onTap: (value) => Navigator.of(context).push(MaterialPageRoute(
+                builder: (context) => ParentProfile(
+                      parent: widget.parent,
+                    ))),
+            items: const [
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.person_outline_rounded), label: "Профиль"),
+              BottomNavigationBarItem(
+                  icon: Icon(Icons.accessible_forward_rounded),
+                  label: "Костыль")
+            ]),
       ),
       body: ListView(
         children: [
@@ -65,4 +101,80 @@ class _ParentTasksState extends State<ParentTasks> {
       ),
     );
   }
+}
+
+Future<void> addChildDialogBuilder(
+  BuildContext context,
+) {
+  final nameController = TextEditingController();
+  return showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        actionsAlignment: MainAxisAlignment.center,
+        content: Column(
+          children: [
+            const Text('Введите данные ребёнка'),
+            InputBlock(
+              label: "Имя ребёнка",
+              controller: nameController,
+            )
+          ],
+        ),
+        actions: [
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.deepOrange,
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              onPressed: () {
+                // nameController.dispose();
+                Navigator.of(context).pop();
+              },
+              child: const Text("Отменить")),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.green,
+              textStyle: Theme.of(context).textTheme.labelLarge,
+            ),
+            child: const Text(
+              'Добавить',
+            ),
+            onPressed: () {
+              generateCodeDialogBuilder(context, nameController.text)
+                  .whenComplete(() {
+                // nameController.dispose();
+                Navigator.of(context).pop();
+              });
+            },
+          )
+        ],
+      );
+    },
+  );
+}
+
+Future<void> generateCodeDialogBuilder(BuildContext context, String childName) {
+  int code = Random().nextInt(899999) + 10000;
+  addChild(childName, code.toString());
+  return showDialog(
+    context: context,
+    builder: (BuildContext context) {
+      return AlertDialog(
+        actionsAlignment: MainAxisAlignment.center,
+        content: Text(code.toString()),
+        actions: [
+          ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.deepOrange,
+                textStyle: Theme.of(context).textTheme.labelLarge,
+              ),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text("Готово"))
+        ],
+      );
+    },
+  );
 }
--- lib/ui/user_profile.dart
+++ lib/ui/user_profile.dart
@@ -3,8 +3,14 @@ import 'package:flutter/material.dart';
 class UserProfileCard extends StatelessWidget {
   final String userName;
   final IconData userProfilePic;
+  final String? code;
+  final String? balance;
   const UserProfileCard(
-      {required this.userName, required this.userProfilePic, Key? key})
+      {required this.userName,
+      required this.userProfilePic,
+      this.code,
+      this.balance,
+      Key? key})
       : super(key: key);
 
   @override
@@ -15,7 +21,11 @@ class UserProfileCard extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Icon(userProfilePic, size: 100),
-            Text(userName, style: const TextStyle(fontSize: 24)),
+            Column(children: [
+              Text(userName, style: const TextStyle(fontSize: 24)),
+              if (balance != null) Text("Баланс: ${balance!}"),
+              if (code != null) Text("Код: ${code!}"),
+            ]),
           ],
         )
       ],
